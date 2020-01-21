873350f43c0d07224d45ad2d2a6e2865a80eaf97
Khaja Moinuddin Mohammed <moinuddinbtech@gmail.com>
2020-01-21 22:13:22 +0530

    Initial Commit

---
+++ EAWinAppDriverTest/Hooks/TestInitialize.cs
@@ -0,0 +1,38 @@
+﻿using EAWinAppDriverTest.Base;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace EAWinAppDriverTest.Hooks
+{
+
+    [Binding]
+    public class TestInitialize
+    {
+
+        [BeforeScenario]
+        public void TestSetup()
+        {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.SetCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+
+            BasePage.WindowsDriver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), desiredCapabilities);
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            if(BasePage.WindowsDriver != null)
+            {
+                BasePage.WindowsDriver.Close();
+                BasePage.WindowsDriver.Quit();
+                BasePage.WindowsDriver = null;
+            }
+        }
+    }
+}
---
+++ EAWinAppDriverTest/Steps/CalculatorSteps.cs
@@ -0,0 +1,52 @@
+﻿using EAWinAppDriverTest.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace EAWinAppDriverTest.Steps
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+
+        CalcPage calcPage = new CalcPage();
+
+        [Given(@"I open the application")]
+        public void GivenIOpenTheApplication()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I click following numbers")]
+        public void GivenIClickFollowingNumbers(Table table)
+        {
+            calcPage.ClickFollowingNumber(table);
+        }
+
+        [Given(@"I perform Addition operation")]
+        public void GivenIPerformAdditionOperation()
+        {
+            calcPage.ClickPlus();
+        }
+
+        [Given(@"I click Equals")]
+        public void GivenIClickEquals()
+        {
+            calcPage.ClickEquals();
+        }
+
+        [Then(@"I should see the result as (.*)")]
+        public void ThenIShouldSeeTheResultAs(int result)
+        {
+            var actual = calcPage.GetResult();
+            string formatResult = $"Display is {result}";
+            //Assert.That(actual, Is.EqualTo(formatResult), "Result is not correct");
+        }
+
+
+    }
+}
